ecb93cc7f894aeef9a1b6783b7a43ece2e005125
Sean Hellebusch <sahellebusch@gmail.com>
2024-12-13 07:30:36 -0600

    add new service calss

--- src/ehr/application/services/ehr.service.ts
+++ src/ehr/application/services/ehr.service.ts
@@ -1,27 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { EHRProviderPort } from '../../domain/ports/ehr-provider.port';
 import { Patient } from '../../domain/entities/patient.entity';
-import { GetCernerPatientUseCase } from '../use-cases/cerner/get-patient.use-case';
-import { GetEpicPatientUseCase } from '../use-cases/epic/get-patient.use-case';
-import { EHRProviderType } from '../../infra/types/ehr-provider-type.enum';
+import { Condition } from '../../domain/entities/condition.entity';
 
 @Injectable()
 export class EHRService {
   constructor(
-    private readonly getCernerPatientUseCase: GetCernerPatientUseCase,
-    private readonly getEpicPatientUseCase: GetEpicPatientUseCase,
+    private readonly cernerProvider: EHRProviderPort,
+    private readonly epicProvider: EHRProviderPort,
   ) {}
 
-  async getPatient(
-    providerType: EHRProviderType,
+  async getPatient(providerId: string, patientId: string): Promise<Patient> {
+    const provider = this.getProvider(providerId);
+    return provider.getPatient(patientId);
+  }
+
+  async getPatientConditions(
+    providerId: string,
     patientId: string,
-  ): Promise<Patient> {
-    switch (providerType) {
-      case EHRProviderType.CERNER:
-        return this.getCernerPatientUseCase.execute({ patientId });
-      case EHRProviderType.EPIC:
-        return this.getEpicPatientUseCase.execute({ patientId });
+  ): Promise<Condition[]> {
+    const provider = this.getProvider(providerId);
+    return provider.getPatientConditions(patientId);
+  }
+
+  private getProvider(providerId: string): EHRProviderPort {
+    switch (providerId.toLowerCase()) {
+      case 'cerner':
+        return this.cernerProvider;
+      case 'epic':
+        return this.epicProvider;
       default:
-        throw new Error(`Unsupported EHR provider: ${providerType}`);
+        throw new Error(`Unsupported EHR provider: ${providerId}`);
     }
   }
 }
